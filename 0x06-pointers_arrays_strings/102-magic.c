418171bbed077dc200d59cab21bd3944c6ffb2d6
festilla <festusndame@gmail.com>
2023-09-22 02:23:43 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-include "main.h"
+int main(void) 
 {
-		int n;
-		int a[5];
-		int *p;
+		int a[] = {4, 8, 98, 42};
+		int *p = a;
 
-		a[2] = 1024;
-		p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-		*(p + 5) = 98
-		/* ...so that this prints 98\n */
+		/*
+		* write your line of code here...
+		* Remember:
+		* - you are not allowed to use a
+		* - you are not allowed to modify p
+		* - only one statement
+		* - you are not allowed to code anything else than this line of code
+		*/
+		*(p + 2) = 98;
 		printf("a[2] = %d\n", a[2]);
 		return (0);
 }
