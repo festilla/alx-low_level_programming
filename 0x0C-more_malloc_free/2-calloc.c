ddb89966aa62fad02aba87ae328e178e79c5eeae
festilla <festusndame@gmail.com>
2023-10-06 07:02:15 -0700

    More malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _memset - Fills memory with a constant byte.
+* @s: The memory area to be filled.
+* @b: The character to copy.
+* @n: The number of times to copy b.
+*
+* Return: A pointer to the memory area s.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
+/**
+* _calloc - Allocates memory for an array.
+* @nmemb: The number of elements in the array.
+* @size: The size of each element.
+*
+* Return: A pointer to the allocated memory.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+return (NULL);
+_memset(ptr, 0, nmemb * size);
+return (ptr);
+}
