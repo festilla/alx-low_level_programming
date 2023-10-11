376bbc7c7f20475d2ace91beaffb456bb3b11cc1
festilla <festusndame@gmail.com>
2023-10-11 04:50:52 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,28 +4,29 @@
 #include "3-calc.h"
 
 /**
-* main - Perform basic arithmetic operations and print the result.
-* @argc: The number of arguments provided to the program.
-* @argv: An array of pointers to the program's arguments.
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
 *
 * Return: Always 0.
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 int num1, num2;
-char *operator;
+char *op;
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
 num1 = atoi(argv[1]);
-operator = argv[2];
+op = argv[2];
 num2 = atoi(argv[3]);
-if (get_op_func(operator) == NULL || operator[1] != '\0')
+if (get_op_func(op) == NULL || op[1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
+if ((*op == '/' && num2 == 0) ||
+(*op == '%' && num2 == 0))
 {
